4f31adab2482df3f5eda10562b073dacb70335f1
bhanuteja6 <bhanuteja6@gmail.com>
2012-10-31 10:46:28 +0530

    added radio buttons check box

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,7 @@
 class Profile < ActiveRecord::Base
   attr_accessible :aboutme, :dob, :email, :file, :gender, :languages, :name, :picture
+  has_attached_file :picture,
+  :styles => {
+       :thumb=> "100x100#",
+       :small  => "400x400>" }
 end
